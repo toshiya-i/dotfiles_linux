bd808d17cc8b631c11c8b4a99c7ef47aff84024c
toshiya-i <toshiya.i@gmail.com>
2017-06-11 11:00:47 +0900

    oh-my-zshのinstall.shに.zshrcをテンプレートからコピーする処理があるため、同処理はinitialize.shより削除

--- initialize.sh
+++ initialize.sh
@@ -17,7 +17,6 @@ done
 # oh-my-zsh
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 
 # dein
